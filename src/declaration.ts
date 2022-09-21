845c125eeb638eda1167f255726e49c6017711c2
wmxpy <wm@mengw.io>
2022-09-20 22:43:39 -0500

    fix object types

--- src/declaration.ts
+++ src/declaration.ts
@@ -7,6 +7,6 @@
 export const MarkedObjectMixinDeclaration: Record<string, string> = {
 
     keys: "(target: Object) => string[]",
-    values: "<T>(target: {[s:string]: T} | ArrayLike<T>) => T[]",
-    entries: "<T>(target: {[s:string]: T} | ArrayLike<T>) => [string, T][]",
+    values: "<T>(target: {[s:string]: T} | Array<T>) => T[]",
+    entries: "<T>(target: {[s:string]: T} | Array<T>) => [string, T][]",
 };
